a455b349a83a830137a236f16f2d864b518a40d1
keisyansra <keisya.naulia@gmail.com>
2024-09-27 08:12:24 +0700

    soal 2

---
+++ Pertemuan5/fungsi2.php
@@ -0,0 +1,17 @@
+<?php
+function perkenalan($nama, $salam){
+    echo $salam.",";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+perkenalan("keisya", "hallo");
+
+echo "<hr/>";
+
+$saya ="Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya,$ucapanSalam);
+
+?>
