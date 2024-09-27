fd6bc49024a5d6fd05b89f5496b94eb1bbbb9fdb
keisyansra <keisya.naulia@gmail.com>
2024-09-27 08:08:03 +0700

    soal 1

---
+++ Pertemuan5/fungsi.php
@@ -0,0 +1,10 @@
+<?php
+
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Keisya<br/>";
+    echo "Senang berkenalan dengan anda <br/>";
+}
+
+perkenalan();
+?>
