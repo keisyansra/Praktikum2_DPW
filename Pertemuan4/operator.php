48da8dd2837e320cb63724c4e42b37f2a8ac4b6b
keisyansra <keisya.naulia@gmail.com>
2024-09-30 19:55:08 +0700

    soal 3.1

--- Pertemuan4/operator.php
+++ Pertemuan4/operator.php
@@ -8,3 +8,10 @@ $hasilKali = $a * $b;
 $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
+
+echo "Hasil Penjumlahan : $hasilTambah";
+echo "Hasil Kurang : $hasilKurang";
+echo "Hasil Kali : $hasilKali";
+echo "Hasil Bagi : $hasilBagi";
+echo "Hasil Sisa Bagi : $sisaBagi";
+echo "Hasil Pangkat : $pangkat";
