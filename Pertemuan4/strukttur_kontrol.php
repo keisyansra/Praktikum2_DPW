1f4b806742bf0202f3ea21cff5fc50f5556dee97
keisyansra <keisya.naulia@gmail.com>
2024-09-30 22:26:10 +0700

    soal 4.3

--- Pertemuan4/strukttur_kontrol.php
+++ Pertemuan4/strukttur_kontrol.php
@@ -25,4 +25,15 @@ while ($jarakSaatIni < $jarakTarget) {
 }
 echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak
 500 kilometer <br><br>";
+
+echo "<b>FOR</b><br>";
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+echo "Jumlah buah yang akan dipanen adalah : $jumlahBuah";
 ?>
