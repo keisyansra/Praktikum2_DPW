a0b9efd78eaa7fa2b54909f381258c31b1fd7524
keisyansra <keisya.naulia@gmail.com>
2024-10-02 13:43:40 +0700

    soal 4.7

---
+++ Pertemuan4/soal2_strukturKontrol.php
@@ -0,0 +1,10 @@
+<?php
+
+$totalBelanja = 120000;
+$diskon = 20;
+$hitungDiskon = $totalBelanja * ($diskon / 100);
+
+echo "Hasil total diskon yang didapatkan pembeli adalah 
+Rp" . $hitungDiskon;
+
+?>
