98b96b2f24f8944c98846fd1eddcd2019324e143
keisyansra <keisya.naulia@gmail.com>
2024-10-03 09:41:28 +0700

    soal 8

--- Pertemuan5/menuBertingkat.php
+++ Pertemuan5/menuBertingkat.php
@@ -33,12 +33,18 @@ $menu = [
     ]
 ];
 
-function tampilkanMenuBertingkat (array $menu){
-    echo "<ul>";
-    foreach ($menu as $key => $item){
-        echo "<li>{$item['nama']}</li>";
+function tampilkanMenuBertingkat (array $menu, $level = 0){
+    echo str_repeat("\t", $level) . "<ul>";
+    foreach ($menu as $item){
+        echo str_repeat("\t", $level + 1). "<li>";
+        echo $item['nama'];
+
+        if (isset($item['submenu'])){
+            tampilkanMenuBertingkat($item['submenu'], $level + 1);
+        }
+        echo "</li>";
     }
-    echo "</ul>";
+    echo str_repeat("\t", $level). "</ul>";
 }
 
 tampilkanMenuBertingkat($menu);
