259592482a279583caa498e99183ddb721a2358d
keisyansra <keisya.naulia@gmail.com>
2024-10-03 10:15:44 +0700

    soal 10

---
+++ Pertemuan5/string2.php
@@ -0,0 +1,11 @@
+<?php
+echo "Baris\nbaru <br>";
+echo "Baris\nbaru <br>";
+echo "Halo\rdunia <br>";
+echo "Halo\rDunia <br>";
+
+echo "<pre>Halo\tDunia!</pre>";
+echo "<pre>Halo\tDunia!</pre>";
+
+echo "katakanlah \"Tidak narkoba!\" <br>";
+echo "katakanlah \"Tidak narkoba!\" <br>";
