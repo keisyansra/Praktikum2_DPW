650dea9560793c5f34b58c953c2213289db73fc3
keisyansra <keisya.naulia@gmail.com>
2024-10-04 09:18:26 +0700

    soal 1

---
+++ Array PHP/array1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+        $listDosen = ["elok nur hamdana","unggul pamenang","bagas nugraha"];
+        echo $listDosen[2] . "<br>";
+        echo $listDosen[0] . "<br";
+        echo $listDosen[1] . "<br";
+        ?>
+    </body>
+</html>
