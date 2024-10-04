97f2ee7aaccb1c94ef0ea19c0acd9453f9a6e439
keisyansra <keisya.naulia@gmail.com>
2024-10-04 09:49:20 +0700

    soal 1

--- Array PHP/array1.php
+++ Array PHP/array1.php
@@ -8,8 +8,14 @@
         <?php
         $listDosen = ["elok nur hamdana","unggul pamenang","bagas nugraha"];
         echo $listDosen[2] . "<br>";
-        echo $listDosen[0] . "<br";
-        echo $listDosen[1] . "<br";
+        echo $listDosen[0] . "<br>";
+        echo $listDosen[1] . "<br><br>";
+
+        echo "<b>Menggunakan Looping</b><br>";
+        foreach ($listDosen as $dosen){
+            echo $dosen;
+            echo "<br>";
+        }
         ?>
     </body>
 </html>
