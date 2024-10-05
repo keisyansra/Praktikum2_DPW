9feac673f8d4b66d7627e7243f589b9765a5842e
keisyansra <keisya.naulia@gmail.com>
2024-10-05 11:16:04 +0700

    soal 2

---
+++ Array PHP/associative.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $dosen = [
+            'nama' => 'elok nur hamdana',
+            'domisili' => 'malang',
+            'jenis_kelamin' => 'perempuan'
+        ];
+        echo "Nama : {$dosen ['nama']} <br>";
+        echo "Domisili : {$dosen ['domisili']} <br>";
+        echo "Jenis Kelamin : {$dosen ['jenis_kelamin']}";
+        ?>
+    </body>
+</html>
