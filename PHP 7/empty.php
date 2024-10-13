b1f012f2fadf530f805d978e9c0f95f62632d95b
keisyansra <keisya.naulia@gmail.com>
2024-10-13 07:02:02 +0700

    soal 2.1

---
+++ PHP 7/empty.php
@@ -0,0 +1,8 @@
+<?php
+$myArray = array(); //array kosong
+if (empty($myArray)) {
+    echo "Array tidak terdefinisikan atau kosong";
+} else {
+    echo "Array terdefinsiikan dan tidak kosong.";
+}
+?>
