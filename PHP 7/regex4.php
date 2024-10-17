c5ee7ced7d0d62de7096d35dfcf396beff7a65e3
keisyansra <keisya.naulia@gmail.com>
2024-10-17 17:37:23 +0700

    soal 5.3

---
+++ PHP 7/regex4.php
@@ -0,0 +1,9 @@
+<?php
+$pattern = '/go[?*]d/';
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan : " .$matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
+?>
